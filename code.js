6ccd3fee5b6a6e94dcded3faf067ea589be8080f
kenzieadmin <kenzieadmin@users.noreply.github.com>
2023-06-03 04:18:48 +0000

    deploy: 4fe8f8e401fd7b1082a6416d112c5ac535357108

--- code.js
+++ code.js
@@ -1,10 +1,16 @@
 // Your Code Here!
 
+// Step One - Combine the two data sets
+
 //find movies data titles matching movieDetails data titles
 const moviesTitlesMatchingDetailsTitles = movieDetails.map(detail => movies.find(movieCopy => movieCopy.title === detail.title)).filter(title => title !== undefined)
 
 // create a deep copy of moviesTitlesMatchingDetailsTitles
 let combinedData = []
+const moviesContainer = document.createElement('div')
+let moviesByInputResult = []
+
+
 
 const createMatchedDataCopy = function (data) {
 
@@ -47,6 +53,8 @@ const combineDatasFunction = function () {
 
 combineDatasFunction()
 
+// Step Two - Render the movies
+
 // displaying searching part of the page and title
 function displaySearchContainer() {
     document.body.textContent = ''
@@ -56,7 +64,7 @@ function displaySearchContainer() {
     pageTitle.innerText = "Kenzie Movie Catalog"
 
 
-    const searchByMoviesTitle = `<label for="moviesTitleId">Search by Movie Title: <input type = "text" id ="moviesTitleId" name = "moviesTitle" placeholder = "Enter a movie title" autocomplete = "off"></label>`
+    const searchByMoviesTitle = `<label for="movieTitleId">Search by Movie Title: <input type = "text" id ="movieTitleId" name = "movieTitle" placeholder = "Enter a movie title" autocomplete = "off"></label>`
     const searchByActors = `<label for="actorsNameId">Search by Actor/Actress: <input type = "text" id ="actorsNameId" name = "actorsName" placeholder = "Enter an actor name" autocomplete = "off"></label>`
     const submitButton = `<button>Search</button>`
 
@@ -64,19 +72,22 @@ function displaySearchContainer() {
     searchMoviesContainer.innerHTML += searchByActors
     searchMoviesContainer.innerHTML += submitButton
 
-
     document.body.append(pageTitle, searchMoviesContainer)
 
 }
 
 displaySearchContainer()
 
-
-// display all movies by clicking the seach button
 const submitButton = document.querySelector('button');
 
+// display all movies by clicking the seach button
+
 submitButton.addEventListener('click', () => {
-   
+
+    moviesContainer.textContent = ''
+    document.getElementById('actorsNameId').value = ''
+    document.getElementById('movieTitleId').value = ''
+    moviesByInputResult = []
     displayMovies()
 })
 
@@ -84,7 +95,6 @@ submitButton.addEventListener('click', () => {
 // display movies function
 function displayMovies() {
 
-    const moviesContainer = document.createElement('div')
     moviesContainer.setAttribute('class', 'movies-container')
 
     for (let film of combinedData) {
@@ -97,16 +107,16 @@ function displayMovies() {
         let castMembersNode = document.createElement('p')
         let genresNode = document.createElement('p')
         let yearNode = document.createElement('p')
-        let popularityNode = document.createElement('h4')
+        let popularityNode = document.createElement('p')
         let imageNode = document.createElement('img')
 
         // add contents to nodes
         imageNode.src = film.imageUrl
-        titleNode.innerText = film.title
+        titleNode.innerText = `Title: ${film.title}`
         castMembersNode.innerText = film.cast
-        genresNode.innerText = film.genres
-        yearNode.innerText = film.year
-        popularityNode.innerText = `Popularity: ${film.popularity}`
+        genresNode.innerHTML = `<strong>genres:</strong> ${film.genres}`
+        yearNode.innerHTML = `<strong>Year:</strong> ${film.year}`
+        popularityNode.innerHTML = `<strong>Popularity:</strong> ${film.popularity}`
 
         // showing up on the screen
         movieCard.append(imageNode, titleNode, castMembersNode, yearNode, genresNode, popularityNode)
@@ -114,3 +124,145 @@ function displayMovies() {
         document.body.append(moviesContainer)
     }
 }
+
+
+
+// Step Three - Searching through movies
+
+document.body.addEventListener('input', (event) => {
+
+    const actorsInput = document.getElementById('actorsNameId')
+    const titleInput = document.getElementById('movieTitleId')
+    const moviesContainer = document.querySelector('.movies-container')
+
+
+    // setTimeout(() => {
+        if (event){
+            combinedData.forEach(movie => {
+
+                let actorsOfMovie = movie.cast.join('')
+                let titleOfMovie = movie.title
+
+                if ((actorsOfMovie.toLowerCase().indexOf(actorsInput.value.toLowerCase()) > -1) && !titleInput.value) {
+
+                    moviesByInputResult.push(movie)
+                    console.log('le')
+                } else if (titleOfMovie.toLowerCase().indexOf(titleInput.value.toLowerCase() > -1) && !actorsInput.value) {
+
+                    moviesByInputResult.push(movie)
+                    console.log('la')
+
+                } else if ((actorsOfMovie.toLowerCase().indexOf(actorsInput.value.toLowerCase()) > -1) && titleOfMovie.toLowerCase().indexOf(titleInput.value.toLowerCase() > -1) && actorsInput.value && titleInput.value) {
+                    moviesByInputResult.push(movie)
+                    console.log('il')
+
+                }
+
+                if (actorsInput.value === '' && titleInput.value === '') {
+                    moviesContainer.textContent = ''
+                    moviesByInputResult = []
+                }
+            })
+
+
+            moviesByInputResult.forEach(movieFound => {
+
+                if (moviesContainer.childElementCount > moviesByInputResult.length) {
+                    moviesContainer.textContent = ''
+                }
+
+                // create nodes
+                let movieCard = document.createElement('div')
+                movieCard.setAttribute('class', 'movie-card')
+
+                let titleNode = document.createElement('h3')
+                let castMembersNode = document.createElement('p')
+                let genresNode = document.createElement('p')
+                let yearNode = document.createElement('p')
+                let popularityNode = document.createElement('h4')
+                let imageNode = document.createElement('img')
+
+                // add contents to nodes
+                imageNode.src = movieFound.imageUrl
+                titleNode.innerText = `Title: ${movieFound.title}`
+                castMembersNode.innerText = movieFound.cast
+                genresNode.innerText = `genres: ${movieFound.genres}`
+                yearNode.innerText = `Year: ${movieFound.year}`
+                popularityNode.innerText = `Popularity: ${movieFound.popularity}`
+
+                // showing up on the screen
+                movieCard.append(imageNode, titleNode, castMembersNode, yearNode, genresNode, popularityNode)
+                moviesContainer.append(movieCard)
+            })
+        }
+        
+    // }, 1000)
+
+
+    // combinedData.forEach(movie => {
+
+    //     let actorsOfMovie = movie.cast.join('')
+    //     let titleOfMovie = movie.title
+
+    //     if ((actorsOfMovie.toLowerCase().indexOf(actorsInput.value.toLowerCase()) > -1) && !titleInput.value){
+
+
+
+    //         moviesByInputResult.push(movie)
+    //         console.log('le')
+
+    //     } else if (titleOfMovie.toLowerCase().indexOf(titleInput.value.toLowerCase() > -1) && !actorsInput.value){
+
+
+    //         moviesByInputResult.push(movie)
+    //         console.log('la')
+
+
+    //     } else if ((actorsOfMovie.toLowerCase().indexOf(actorsInput.value.toLowerCase()) > -1) && titleOfMovie.toLowerCase().indexOf(titleInput.value.toLowerCase() > -1) && actorsInput.value && titleInput.value){
+
+
+
+    //         moviesByInputResult.push(movie)
+    //         console.log('il')
+
+    //     }
+
+    //     if (actorsInput.value === '' && titleInput.value === '') {
+    //         moviesContainer.textContent = ''
+    //         moviesByInputResult = []
+    //     }
+    // })
+
+    // moviesByInputResult.forEach(movieFound => {
+
+    //     if (moviesContainer.childElementCount > moviesByInputResult.length) {
+    //         moviesContainer.textContent = ''
+    //     }
+
+    //     // create nodes
+    //     let movieCard = document.createElement('div')
+    //     movieCard.setAttribute('class', 'movie-card')
+
+    //     let titleNode = document.createElement('h3')
+    //     let castMembersNode = document.createElement('p')
+    //     let genresNode = document.createElement('p')
+    //     let yearNode = document.createElement('p')
+    //     let popularityNode = document.createElement('h4')
+    //     let imageNode = document.createElement('img')
+
+    //     // add contents to nodes
+    //     imageNode.src = movieFound.imageUrl
+    //     titleNode.innerText = `Title: ${movieFound.title}`
+    //     castMembersNode.innerText = movieFound.cast
+    //     genresNode.innerText = `genres: ${movieFound.genres}`
+    //     yearNode.innerText = `Year: ${movieFound.year}`
+    //     popularityNode.innerText = `Popularity: ${movieFound.popularity}`
+
+    //     // showing up on the screen
+    //     movieCard.append(imageNode, titleNode, castMembersNode, yearNode, genresNode, popularityNode)
+    //     moviesContainer.append(movieCard)
+    // })
+
+})
+
+console.log(combinedData)
